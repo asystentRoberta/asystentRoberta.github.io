9806b7f8345718eda17af764919fbe4c762836a7
Robert <asystent.roberta@gmail.com>
2019-01-11 17:56:47 +0100

    Delete functionality was broken.

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ $(document).ready(function() {
     function handleTaskDeleteRequest() {
         var parentEl = $(this).parents('[data-task-id]');
         var taskId = parentEl.attr('data-task-id');
-        var requestUrl = apiRoot + 'deleteTask';
+        var requestUrl = apiRoot + 'deleteTaskById';
 
         $.ajax({
             url: requestUrl + '/?' + $.param({
